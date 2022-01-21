c32ec75d53f61fddf691104fc9d8f9aa0167495b
HasanAhmed987 <92812904+HasanAhmed987@users.noreply.github.com>
2022-01-22 02:43:00 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import './App.css';
+import NavBar from "./NavBar";
+import Counters from "./components/Counters";
 
 function App() {
   return (
     <div className="App">
-      
+      <NavBar />
+      <main className="container">
+        <Counters />
+      </main>
     </div>
   );
 }
