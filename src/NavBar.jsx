d0b5e7dd1006d9627c044305a989d5432d1efd98
HasanAhmed987 <92812904+HasanAhmed987@users.noreply.github.com>
2022-01-22 02:46:54 +0500

    Add files via upload

---
+++ src/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function NavBar() {
+  return (
+    <div>
+      <nav className="navbar navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Counter
+          </a>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default NavBar;
