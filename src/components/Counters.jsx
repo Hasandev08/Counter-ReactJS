568ada9866a9bc96f9074c7a044617f4ccbddfc5
HasanAhmed987 <92812904+HasanAhmed987@users.noreply.github.com>
2022-01-22 02:45:42 +0500

    Update Counters.jsx

--- src/components/Counters.jsx
+++ src/components/Counters.jsx
@@ -4,10 +4,10 @@ import Counter from "./Counter";
 class Counters extends Component {
   state = {
     counter: [
-      { id: 1, value: 4 },
-      { id: 2, value: 1 },
+      { id: 1, value: 0 },
+      { id: 2, value: 0 },
       { id: 3, value: 0 },
-      { id: 4, value: 2 },
+      { id: 4, value: 0 },
     ],
   };
 
