d103431bc255b421828f70c48669a7642d66ebde
HasanAhmed987 <92812904+HasanAhmed987@users.noreply.github.com>
2022-01-22 02:44:02 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import Counters from "./components/Counters";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Counters />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
